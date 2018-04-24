55fc868b0e5c59822dbb3ae09b8c82a7389a1527
Jean-Luc-Adams <jladams91@gmail.com>
2018-04-24 12:34:40 -0500

    Lab Completed

--- scripts.js
+++ scripts.js
@@ -8,10 +8,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     let id = 0;
 
-    button.addEventListener('click', function(){
+    button.addEventListener('click', function () {
         let div = document.createElement('div');
         div.className = 'square';
         id++;
+        div.id = id;
         let p = document.createElement('p');
         let text = document.createTextNode(id);
         p.appendChild(text);
@@ -21,15 +22,47 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.appendChild(div);
 
         p.addEventListener('mouseenter', hover);
+
         function hover() {
-            p.style.opacity= '1';
+            p.style.opacity = '1';
         }
 
         p.addEventListener('mouseleave', noHover);
+
         function noHover() {
-            p.style.opacity=('0');
+            p.style.opacity = ('0');
         }
-            
+
+        //Random colors for the squares
+        let colors = ['silver', 'red', 'teal', 'blue', 'limegreen', 'gold', 'pink'];
+
+        //Changes the squares to a random color when clicked
+        div.addEventListener('click', function () {
+            let randomColor = Math.floor(Math.random() * colors.length);
+            div.style.backgroundColor = colors[randomColor];
+        })
+
+        div.addEventListener('dblclick', divDoubleClick); 
+        function divDoubleClick() {
+            if (div.id % 2 == 0) {
+              let squareId = parseInt(div.id) + 1;
+              let square = document.getElementById(squareId);
+              if (square == null) {
+                  alert('This is not the square you are looking for');
+              } else {
+                    square.parentNode.removeChild(square);
+              } 
+        }   else {
+            let squareId = parseInt(div.id) - 1;
+            let square = document.getElementById(squareId);
+            if(square == null) {
+                alert('No, no, Mr. Square no here...');
+            } else {
+                square.parentNode.removeChild(square);
+            }
+        }
+    }
+
     })
-    
+
 })
